2a80f16d00fee4259c22c5aee57fc80e4bf37585
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2021-04-25 19:29:16 -0700

    tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,26 +1,45 @@
-import Test.HUnit
-import Test.Framework.Providers.HUnit
-import Test.Framework.Runners.Console
 import Data.Monoid
 import Control.Monad
 -- import Utils
-import Test.QuickCheck
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+import Test.Tasty.HUnit
 import Lib
 
 sut :: Enigma
 sut = createEnigma ["BDFHJLCPRTXVZNYEIWGAKMUSQO","AJDKSIRUXBLHWTMCQGZNPYFVOE","EKMFLGDQVZNTOWYHXUSPAIBRCJ"] "YRUHQSLDPXNGOKMIEBFZCWVJAT"
 
+genAlphabetChar :: Gen Char
+genAlphabetChar = elements ['A'..'Z']
+
 singleCharacterTest :: Assertion
 singleCharacterTest = do
   let c = 'A'
+  let b = singleCharacterProperty c
+  assertBool "Encoded character should decode to original starting character" b
+
+singleCharacterProperty :: Char -> Bool
+singleCharacterProperty c = do
   let (a,_) = encode sut c
   let (b,_) = encode sut a
-  assertEqual "Encoded character should decode to original starting character" c b
+  c == b
+  
 
 main :: IO ()
-main = defaultMainWithOpts
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [properties, unitTests]
+
+properties :: TestTree
+properties = testGroup "Properties" [qcProps]
+
+unitTests = testGroup "Unit tests"
   [
     testCase "singleCharacterTest" singleCharacterTest
   ]
-  mempty
 
+qcProps = testGroup "(checked by QuickCheck)"
+  [ 
+    QC.testProperty "singleCharacterTest" $ QC.forAll (elements ['A'..'Z']) $ \c -> singleCharacterProperty c
+  ]
