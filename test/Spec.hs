a4fa2586286e12f74840405a4f7551e93df9d458
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2021-05-07 23:41:43 -0700

    tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,15 +6,6 @@ import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
 import Lib
 
-sut :: Enigma
-sut = 
-  createEnigmaWithRotors [r3, r2, r1] rfl
-    where
-      r1 = rotor_I 0
-      r2 = rotor_I 0
-      r3 = rotor_I 0
-      rfl = reflector_B
-
 enigmaAllRotor1 :: Enigma
 enigmaAllRotor1 = 
   createEnigmaWithRotors [r3, r2, r1] rfl
@@ -33,6 +24,15 @@ enigmaAllRotor1_StartFirstRotorAtZ =
       r3 = rotor_I 25
       rfl = reflector_B
 
+enigmaConfig2 :: Enigma
+enigmaConfig2 = 
+  createEnigmaWithRotors [r3, r2, r1] rfl
+    where
+      r1 = rotor_III 0
+      r2 = rotor_II 0
+      r3 = rotor_I 0
+      rfl = reflector_B
+      
 genAlphabetChar :: Gen Char
 genAlphabetChar = elements ['A'..'Z']
 
@@ -62,28 +62,41 @@ cipheredExample_2_Test = do
 
 cipheredExample_3_Test :: Assertion
 cipheredExample_3_Test = do
+  let sut = enigmaAllRotor1
   let plainText = take 26 (repeat 'A')
   let result = evalState (encode plainText) sut
   assertEqual "Example did not encode to expected value. 2nd rotor should have rotated once." "UOTGRLFGERCPELTTVTJBGHVWPY" result
 
 cipheredExample_4_Test :: Assertion
 cipheredExample_4_Test = do
+  let sut = enigmaAllRotor1
   let plainText = take 52 (repeat 'A')
   let result = evalState (encode plainText) sut
   assertEqual "Example did not encode to expected value. 2nd rotor should have rotated once." "UOTGRLFGERCPELTTVTJBGHVWPYEUMJYTVIZFNVFWYFJVOBTTNQRU" result
 
+cipheredExample_5_Test :: Assertion
+cipheredExample_5_Test = do
+  let sut = enigmaConfig2
+  let plainText = take 52 (repeat 'A')
+  let result = evalState (encode plainText) sut
+  assertEqual "Example did not encode to expected value. 2nd rotor should have rotated once." "FTZMGISXIPJWGDNJJCOQTYRIGDMXFIESRWZGTOIUIEKKDCSHTPYO" result
+
+
 singleCharacterNeverEncodesToItselfProperty :: AlphabetChar -> Property
 singleCharacterNeverEncodesToItselfProperty (AlphabetChar c) = True ==> do
+  let sut = enigmaAllRotor1
   let a = evalState (encode [c]) sut
   c /= head a
 
 encodedStringShouldNotBeOriginalStringProperty :: AlphabetString -> Property
 encodedStringShouldNotBeOriginalStringProperty (AlphabetString s) = True ==> do
+  let sut = enigmaAllRotor1  
   let a = evalState (encode s) sut
   s /= a
 
 encodedStringShouldDecodeToItselfProperty :: AlphabetString -> Property
 encodedStringShouldDecodeToItselfProperty (AlphabetString s) = True ==> do
+  let sut = enigmaAllRotor1
   let a = evalState (encode s) sut
   let b = evalState (encode a) sut
   s == b
@@ -104,6 +117,7 @@ unitTests = testGroup "Unit tests"
     , testCase "cipheredExample_2_Test" cipheredExample_2_Test
     , testCase "cipheredExample_3_Test" cipheredExample_3_Test
     , testCase "cipheredExample_4_Test" cipheredExample_4_Test
+    , testCase "cipheredExample_5_Test" cipheredExample_5_Test
   ]
 
 qcProps = testGroup "(checked by QuickCheck)"
